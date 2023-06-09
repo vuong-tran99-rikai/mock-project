f31f0e52912083b144de3e8ba2ec0271a28551f4
“Vuong <vuong.tran@rikai.technology>
2023-06-09 15:09:36 +0700

    add signup, show all users and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :users
+  get '/destroy', to: 'users#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
