56815acd6e7b39d61dc69aba2e0ae448e3444396
“Vuong <vuong.tran@rikai.technology>
2023-06-09 16:24:23 +0700

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :users
-  get '/login', to: 'session#new' 
-  post '/login', to: 'session#create'
-  get '/signup', to: 'users#new' 
+  get '/destroy', to: 'users#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
