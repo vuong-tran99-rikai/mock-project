9f4a367361047e8a138bbd0c91f97f557ef313f4
TienDatVo <dat.vo@rikai.technology>
2023-06-12 11:29:09 +0700

    up create

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :users
   resources :categories
+  resources :books
   get '/create-categories', to: 'categories#new'
   post '/create-categories', to: 'categories#create'
+  get '/create-books', to: 'books#new'
+  post '/create-books', to: 'books#create'
 
   get '/login', to: 'session#new' 
   post '/login', to: 'session#create'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,18 @@ require 'faker'
 #              password_confirmation: 'foobar')
 
 
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
+# 99.times do |n|
+#   name = Faker::Name.name
+#   email = "example-#{n + 1}@railstutorial.org"
+#   password = 'password'
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password)
+# end
+99.times do
+  Category.create(
+    name_category: Faker::Lorem.word,
+    status: rand(0..1)
+  )
 end
